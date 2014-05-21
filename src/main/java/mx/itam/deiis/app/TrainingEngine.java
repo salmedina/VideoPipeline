2a0c94105cf17245c8e30e4222c8e5447854cdb5
salmedina <salbed.medina@gmail.com>
2014-05-20 22:00:08 -0500

    - Added classpath
    - Added type system to VW Translator

--- src/main/java/mx/itam/deiis/app/TrainingEngine.java
+++ src/main/java/mx/itam/deiis/app/TrainingEngine.java
@@ -16,7 +16,6 @@ import org.apache.uima.util.XMLInputSource;
 public class TrainingEngine {
 
 	public static void main(String [] args) throws Exception {
-
 		String	sLine;
 		long	startTime = System.currentTimeMillis();
 
