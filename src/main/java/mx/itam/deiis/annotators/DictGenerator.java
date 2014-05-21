5ae46c9b035e1ad69ab4a75c0283d845980e4e5d
salmedina <salbed.medina@gmail.com>
2014-05-21 16:32:50 -0500

    Full integration of Training Pipeline, missing details as: set to spaces instead of Enters for VW files, rename files in VW Translator

--- src/main/java/mx/itam/deiis/annotators/DictGenerator.java
+++ src/main/java/mx/itam/deiis/annotators/DictGenerator.java
@@ -4,6 +4,7 @@ import java.util.Iterator;
 
 import mx.itam.deiis.spark.*;
 import mx.itam.deiis.types.*;
+import mx.itam.deiis.utils.FSTool;
 
 import org.apache.uima.analysis_component.JCasAnnotator_ImplBase;
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
@@ -19,9 +20,9 @@ public class DictGenerator extends JCasAnnotator_ImplBase{
 		//Obtain input annotators
 		// FEAT FILES - load and verify
 		FSIndex featIdx = aJCas.getAnnotationIndex(FeatFiles.type);
-		Iterator FeatIter = featIdx.iterator();
-		if(!FeatIter.hasNext()) {
-			System.out.println("Missing dictionary files annotation");
+		Iterator featIter = featIdx.iterator();
+		if(!featIter.hasNext()) {
+			System.out.println("Missing feature files annotation");
 		}
 		FeatFiles featFiles = (FeatFiles)featIter.next();
 		
@@ -35,35 +36,26 @@ public class DictGenerator extends JCasAnnotator_ImplBase{
 		
 		//Verify paths
 		String featPath = featFiles.getPath();
-		if(!fsTool.dirExists(sourcePath)) {
-			System.out.println("Source directory does not exist.");
+		String dictPath = dictFiles.getPath();
+		if(!FSTool.dirExists(dictPath)) {
+			System.out.println("Dictionary directory does not exist.");
 		}
-		if(!fsTool.dirExists(featPath)) {
+		if(!FSTool.dirExists(featPath)) {
 			System.out.println("Features' directory does not exist.");
 		}
 		
 		long	startTime = System.currentTimeMillis();
-		int k = 60;
-		Dictionary dict;
-		Performance perf;
-		String annotatorID="Dictionary_Generator";
-		//Gaby Paths
-		String sourceFile = "src/main/resources/data/*.sift";
-		String outFile ="src/main/resources/data/dictionary.txt";
-		String objectFile = "src/main/resources/data/kmeans_model.obj";
-
-		/*	
-		//Zal Paths
-		String sourceFile = "src\\main\\resources\\data\\*.sift";
-		String outFile ="src\\main\\resources\\data\\dictionary.txt";
-		String objectFile = "src\\main\\resources\\data\\kmeans_model.obj";
-		 */
+		int 	k = 60;
 		
-		new kmeansCreateDictionary().createDictionary(k, sourceFile, outFile, objectFile);
-		dict=new Dictionary(aJCas);
-		perf=new Performance(aJCas);
-		//dict.setBegin();
-		//dict.setEnd();
+		String annotatorID	= "Dictionary_Generator";
+		String sourceFiles	= featPath + "\\*.sift";
+		String outFile		= dictPath + "\\dictionary.txt";
+		String objectFile	= dictPath + "\\kmeans_model.obj";
+
+		new kmeansCreateDictionary().createDictionary(k, sourceFiles, outFile, objectFile);
+		
+		// Crate DICTIONARY annotation
+		Dictionary dict=new Dictionary(aJCas);
 		dict.setK(k);
 		dict.setObj(objectFile);
 		dict.setTextFile(outFile);
@@ -74,6 +66,8 @@ public class DictGenerator extends JCasAnnotator_ImplBase{
 		System.out.println("====================  Dictionary Generator  =====================");
 		System.out.println("Total time taken: "+totalTime+" segs"+"\n");
 		
+		// Generate PERFORMANCE annotation
+		Performance perf=new Performance(aJCas);
 		perf.setPhase(annotatorID);
 		perf.setExecTime(totalTime);
 		perf.addToIndexes();
